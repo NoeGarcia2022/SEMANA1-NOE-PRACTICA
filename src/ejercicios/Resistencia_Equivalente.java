ea8b08dc04bb9e4ffc6ed768ef7388ba88889231
NoeGarcia2022 <noegarcia4789@gmail.com>
2022-03-12 09:07:53 -0600

    Java Class Resistencia Equivalente

--- src/ejercicios/Resistencia_Equivalente.java
+++ src/ejercicios/Resistencia_Equivalente.java
@@ -1,9 +1,13 @@
 package ejercicios;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 public class Resistencia_Equivalente {
 
     public static void main(String[] args) {
         
+        NumberFormat formato = new DecimalFormat("#0.00");
         //declarar variables
         double R1, R2, R3, R4, req;
         
@@ -12,12 +16,12 @@ public class Resistencia_Equivalente {
         R1 = 5.1;
         R2 = 68;
         R3 = 75;
-        R4 = 84;
+        R4 = 82;
         
         
-        req = ((1/R1)+(1/R2)+(1/R3)+(1/R4));
+        req = (1/R1)+(1/R2)+(1/R3)+(1/R4);
         
-        System.out.println("La Resistencia Equivalente es: " + req);
+        System.out.println("La Resistencia Equivalente es: " + formato.format (req) + " ohm");
     }
     
 }
